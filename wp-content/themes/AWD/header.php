63eb10842b2763e5fbc43251ded3cb1a6750d043
Dmytro Martus <funbrainfun@gmail.com>
2022-06-13 23:13:42 +0300

    lesson num 8

--- wp-content/themes/AWD/header.php
+++ wp-content/themes/AWD/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * header template
  * 
@@ -8,13 +9,17 @@
 
 <!DOCTYPE html>
 <html lang="<?php language_attributes(); ?>">
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' )?>">
+    <meta charset="<?php bloginfo('charset') ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WordPress theme</title>
     <?php wp_head(); ?>
 </head>
+
 <body>
-    <!-- <php? wp_body_open(); ?> -->
+    <?php if (function_exists('wp_body_open')) {
+        wp_body_open();
+    } ?>
     <header class="header">this is my header</header>
