4d08b83f7b4ae706a1b194b7133c218693edd99a
Dmytro Martus <funbrainfun@gmail.com>
2022-06-13 20:35:37 +0300

    lesson num 6

--- wp-content/themes/AWD/functions.php
+++ wp-content/themes/AWD/functions.php
@@ -10,3 +10,12 @@
 // function metrics_analytics() {
 //     //place for google analytics here (if you want)  
 // }
+
+
+
+function awd_add_scripts()
+{
+    wp_enqueue_style('style-sheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+}
+
+add_action('wp_enqueue_scripts', 'awd_add_scripts');
